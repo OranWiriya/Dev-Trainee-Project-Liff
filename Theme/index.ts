e8801a79ef9d5b0d0242c7bfd4e0d278b89c31c4
OranWiriya <megamanoran@gmail.com>
2023-07-11 14:45:03 +0700

    edited bgcolor

--- Theme/index.ts
+++ Theme/index.ts
@@ -14,7 +14,7 @@ const theme = createTheme({
           marginBottom: '2.5rem',
           borderColor: '#06C755',
           color: '#06C755',
-          backgroundColor: 'transparent',
+          backgroundColor: 'white',
           '&, :hover': {
             borderColor: '#06c756f0',
             color: '#06c756f0',
